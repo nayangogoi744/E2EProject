39dd5da9adf9a478c6f24f36e9787d49cef62b6b
Jyoti Gogoi <njy@softwareag.com>
2022-04-11 08:22:25 +0530

    Updated the base class

--- src/main/java/resources/Base.java
+++ src/main/java/resources/Base.java
@@ -20,24 +20,28 @@ public class Base {
 	
 	public WebDriver initializeDriver() throws IOException {
 		prop = new Properties();
-		FileInputStream fis = new FileInputStream("src\\main\\java\\resources\\data.properties");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\data.properties");
 		prop.load(fis);
 		//String browserName = prop.getProperty("browser");
 		String browserName = System.getProperty("browser");
 		System.out.println("Browser is:========="+browserName+"============");
-		if(browserName.equals("chrome")) {
+		if(browserName.contains("chrome")) {
 			System.setProperty("webdriver.chrome.driver",
-					"drivers\\chromedriver.exe");
+					System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+			ChromeOptions options =  new ChromeOptions();
+			if(browserName.contains("headless")) {
+				options.addArguments("headless");
+			}
 			driver = new ChromeDriver();
 			
 		}else if(browserName.equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver",
-					"drivers\\geckodriver.exe");
+					System.getProperty("user.dir")+"\\drivers\\geckodriver.exe");
 			driver = new FirefoxDriver();
 			
 		}else if(browserName.equals("IE")) {
 			System.setProperty("webdriver.ie.driver",
-					"drivers\\IEDriverServer.exe");
+					System.getProperty("user.dir")+"\\drivers\\IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
 		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
